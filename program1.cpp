4e218742146f8e5a50b4b5e26eafc88a7765036a
Michael Quinn <mquinn5@binghamton.edu>
2018-09-24 11:44:25 -0400

    added Filereader and updated program1.cpp and makefile

--- program1.cpp
+++ program1.cpp
@@ -1,8 +1,10 @@
+#include "Filereader.h"
 #include <stdlib.h>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <vector>
+#define DEBUG false;
 
 int main(int argc, char** argv){
 	return 0;
